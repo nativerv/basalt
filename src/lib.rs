854668e6a35554ec26414e071840f2a2366ff1f4
nativerv <nativerv@protonmail.com>
2023-07-09 19:55:44 +0300

    feat: working 'hello world' example

---
+++ src/lib.rs
@@ -0,0 +1,21 @@
+//! Demo app for egui
+//#![allow(clippy::missing_errors_doc)]
+
+mod basalt;
+
+pub use basalt::BasaltApp;
+
+/// Time of day as seconds since midnight. Used for clock in demo app.
+pub(crate) fn seconds_since_midnight() -> f64 {
+    use chrono::Timelike;
+    let time = chrono::Local::now().time();
+    time.num_seconds_from_midnight() as f64 + 1e-9 * (time.nanosecond() as f64)
+}
+
+// ----------------------------------------------------------------------------
+
+#[cfg(target_arch = "wasm32")]
+mod web;
+
+#[cfg(target_arch = "wasm32")]
+pub use web::*;
